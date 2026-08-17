843302301f00392cd5dbd57f809d08da20e6027a
dev-raimundos <devraimundos@outlook.com>
2026-08-17 14:55:55 -0300

    feat: adiciona hostname da aplicação pra produção

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'cestify',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    hostname: 'coeur.app.br',
+    androidScheme: 'https'
+  }
 };
 
 export default config;
